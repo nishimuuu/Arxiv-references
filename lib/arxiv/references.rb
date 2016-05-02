30229a70889d58d983d215c4b288775aeaf39e11
Takahiro Nishimura <takahiro_nishimura@r.recruit.co.jp>
2016-05-02 15:59:32 +0900

    initial commit

---
+++ lib/arxiv/references.rb
@@ -0,0 +1,7 @@
+require "arxiv/references/version"
+
+module Arxiv
+  module References
+    # Your code goes here...
+  end
+end
---
+++ spec/arxiv/references_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Arxiv::References do
+  it 'has a version number' do
+    expect(Arxiv::References::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
