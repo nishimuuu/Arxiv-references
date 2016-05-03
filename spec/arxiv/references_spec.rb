bb62cbfff53c50b6318b20a7d29f6d39d09deb60
Takahiro Nishimura <takahiro_nishimura@r.recruit.co.jp>
2016-05-03 10:35:23 +0900

    test removed

--- spec/arxiv/references_spec.rb
+++ spec/arxiv/references_spec.rb
@@ -5,7 +5,7 @@ describe Arxiv::References do
     expect(Arxiv::References::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+#  it 'does something useful' do
+#    expect(false).to eq(true)
+#  end
 end
